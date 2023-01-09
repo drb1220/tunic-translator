4dcee7f65afd900ebc1bbc4a612b7532f12bbb42
David Bunger <davidbunger@Davids-MacBook-Pro.local>
2023-01-09 13:29:48 -0500

    Added translator.py and updated README.md

--- translator.py
+++ translator.py
@@ -1,4 +1,3 @@
-# Tunic Translator
 # Goals:
 # Convert english into phonetic spelling
 # Map phonetic spellings to symbols
@@ -20,3 +19,21 @@
 # I might just use the IPA standard though, but I'll have to map multiple IPA characters to the same symbol
 # Vowels: ee, a, ih, eh, ah, uu, oo, up, er, ur, ar, or, ow, ay, oh, i, oy, ?
 # Consonants: p, b, t, d, k, g, ch, j, f, v, th, Th, s, z, sh, zh, h, m, n, ng, l, r, y, w
+
+import eng_to_ipa as ipa
+
+def separate_words(phrase):
+    result = []
+    word = ""
+    for letter in phrase:
+        if letter == ' ':
+            if word != "":
+                result.append(word)
+                word = ""
+        else:
+             word += letter
+    if word != "":
+        result.append(word)
+    return result
+
+print(separate_words("deez nuts fortnite battle pass"))
